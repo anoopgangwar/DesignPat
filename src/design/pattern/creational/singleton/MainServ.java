9ed971d92eb74f48b0d14907239e57d3ad75fa35
anoop <anoopgangwar650@gmail.com>
2016-11-26 20:34:20 +0530

    intial commit

--- src/design/pattern/creational/singleton/MainServ.java
+++ src/design/pattern/creational/singleton/MainServ.java
@@ -5,6 +5,7 @@ public class MainServ
 	public static void main(String[] args) 
 	{	
 		// By Static vaiable
+		System.out.println("------------ Eager Intitialization --------------");
 		SingleObjectByStatic sOb = SingleObjectByStatic.getInstance();
 		sOb.setName("Anoop");
 		System.out.println(sOb.getName());
@@ -13,14 +14,27 @@ public class MainServ
 		sOb1.setName("D");
 		System.out.println(sOb.getName());
 		System.out.println(sOb1.getName());
+		System.out.println("------------ Draw Backs : 1) create a class instance eveb though client application might not using it" +
+				" 2) No option of exception handling --------------");
 		// By Static Block
+		System.out.println("------------ Static Block Intitialization --------------");
 		SingletonObjectByStaticBlock sOs = SingletonObjectByStaticBlock.getInstance();
 		sOs.setName("FF");
 		SingletonObjectByStaticBlock sOs1 = SingletonObjectByStaticBlock.getInstance();
 		sOs1.setName("FG");
 		System.out.println(sOs.getName());
 		System.out.println(sOs1.getName());
+		System.out.println("------------ Draw Backs : 1) create a class instance eveb though client application might not using it" +
+				"--------------");
 		// By Lazy Initialization
-		
+		System.out.println("------------ Lazy Intitialization --------------");
+		SingleObjectByLazyInitialization sLs = SingleObjectByLazyInitialization.getInstance();
+		sLs.setName("aa");
+		SingleObjectByLazyInitialization sLs1 = SingleObjectByLazyInitialization.getInstance();
+		sLs1.setName("bb");
+		System.out.println(sLs.getName());
+		System.out.println(sLs1.getName());
+		System.out.println("------------ Draw Backs : 1) create a class instance eveb though client application might not using it" +
+				" 2) No option of exception handling --------------");
 	}
 }
--- src/design/pattern/creational/singleton/SingleObjectByLazyInitialization.java
+++ src/design/pattern/creational/singleton/SingleObjectByLazyInitialization.java
@@ -1,5 +1,30 @@
 package design.pattern.creational.singleton;
 
-public class SingleObjectByLazyInitialization {
+public class SingleObjectByLazyInitialization 
+{
+	private static SingleObjectByLazyInitialization instance;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private SingleObjectByLazyInitialization()
+	{
+		
+	}
+	
+	public static SingleObjectByLazyInitialization getInstance()
+	{
+		if(instance == null)
+		{
+		  instance = new SingleObjectByLazyInitialization();
+		}
+		return instance;
+	}
+	
 }
